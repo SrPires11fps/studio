dfeb91592546e693ff41bf6855cfd8693c660def
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-11 18:30:35 +0000

    na seção de perguntas frequentes, coloque o "devolveremos 100% do seu

--- src/components/landing/Faq.tsx
+++ src/components/landing/Faq.tsx
@@ -21,7 +21,7 @@ const faqs = [
     },
     {
         question: "E se eu não gostar? Qual a garantia?",
-        answer: "Seu risco é zero! Oferecemos uma <strong class=\"font-semibold\">garantia incondicional de 7 dias</strong>. Se por qualquer motivo você não ficar satisfeito com o método, basta nos enviar um e-mail e devolveremos 100% do seu investimento."
+        answer: "Seu risco é zero! Oferecemos uma <strong class=\"font-semibold\">garantia incondicional de 7 dias</strong>. Se por qualquer motivo você não ficar satisfeito com o método, basta nos enviar um e-mail e <strong class=\"font-semibold\">devolveremos 100% do seu investimento</strong>."
     }
 ]
 
