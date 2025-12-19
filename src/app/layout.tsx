da457ab7d96c3a5ce9039d324d214565f5cd61e4
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-19 21:58:39 +0000

    o engraçado é que sempre dispara 11 vezes o page view, e eu não rolo a p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,22 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
+        <Script id="fb-pixel-base" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1201401578841926');
+          `}
+        </Script>
+        <FacebookPixel />
         {children}
         <Toaster />
-        <FacebookPixel />
         <Script
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
@@ -37,6 +50,14 @@ export default function RootLayout({
           async
           defer
         ></Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1201401578841926&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -1,42 +1,18 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export function FacebookPixel() {
   const pathname = usePathname();
 
   useEffect(() => {
-    // This effect ensures that the pixel is only initialized once
+    // This effect tracks page views after the base script is loaded in layout.tsx
     if (window.fbq) {
       window.fbq('track', 'PageView');
-      return;
     }
-    
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-
-    window.fbq('init', '1201401578841926');
-    window.fbq('track', 'PageView');
-    
   }, [pathname]);
 
-  return (
-    <>
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1201401578841926&ev=PageView&noscript=1"
-        />
-      </noscript>
-    </>
-  );
+  // The <noscript> part is in layout.tsx
+  return null;
 }
