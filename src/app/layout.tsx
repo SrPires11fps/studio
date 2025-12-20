ca8a3aa7047551fa5e1abd12e700275978385f5f
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 20:03:19 +0000

    remova o pixel da utmify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,26 +34,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-57RR9X3W');`}}></script>
         {/* End Google Tag Manager */}
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                if (typeof window === 'undefined') return;
-                
-                // Utmify Pixel Script
-                if (!document.querySelector('script[src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"]')) {
-                  window.pixelId = "6946d65085f20f4ee7658aca";
-                  var pixelScript = document.createElement("script");
-                  pixelScript.setAttribute("async", "");
-                  pixelScript.setAttribute("defer", "");
-                  pixelScript.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-                  document.head.appendChild(pixelScript);
-                }
-              })();
-            `,
-          }}
-        ></script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
