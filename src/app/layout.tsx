fd78d2c0e7eba1a0381342ab20dd000c0b3c7771
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 19:47:10 +0000

    remova o script de utm da utmify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import React from 'react';
 import { Poppins } from 'next/font/google';
-import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -44,36 +43,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         
         {children}
         <Toaster />
-
-        <script
-          id="utmify-loader"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                if (document.getElementById('utmify-pixel-script')) return;
-                
-                window.pixelId = "6946d65085f20f4ee7658aca";
-                var pixelScript = document.createElement("script");
-                pixelScript.id = "utmify-pixel-script";
-                pixelScript.async = true;
-                pixelScript.defer = true;
-                pixelScript.src = "https://cdn.utmify.com.br/scripts/pixel/pixel.js";
-                document.head.appendChild(pixelScript);
-
-                if (document.getElementById('utmify-utm-script')) return;
-
-                var utmScript = document.createElement("script");
-                utmScript.id = "utmify-utm-script";
-                utmScript.src = "https://cdn.utmify.com.br/scripts/utms/latest.js";
-                utmScript.setAttribute('data-utmify-prevent-xcod-sck', '');
-                utmScript.setAttribute('data-utmify-prevent-subids', '');
-                utmScript.async = true;
-                utmScript.defer = true;
-                document.head.appendChild(utmScript);
-              })();
-            `,
-          }}
-        />
       </body>
     </html>
   );
