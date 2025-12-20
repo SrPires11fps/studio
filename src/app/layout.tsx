13142c5a41ab0ab2947ed9b4fb2f74c7d4732597
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 15:15:02 +0000

    cacete porra merda filha da puta do caralho. tava dando certo antes sem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import React from 'react';
 import { Poppins } from 'next/font/google';
-import { ThirdPartyScripts } from '@/components/ThirdPartyScripts';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -32,7 +32,32 @@ export default function RootLayout({
         {children}
         <Toaster />
 
-        <ThirdPartyScripts />
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1201401578841926');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+            <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src="https://www.facebook.com/tr?id=1201401578841926&ev=PageView&noscript=1"
+            />
+        </noscript>
       </body>
     </html>
   );
