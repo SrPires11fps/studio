6fde312954860164553b2fd8fe75e3b024d9a418
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 13:59:22 +0000

    problema persiste. tente fazer o contrário, utmify carrega antes e o pix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${poppins.variable}`}>
       <body>
+        
+        {children}
+        <Toaster />
+
+        <Script 
+          id="utmify-script"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          strategy="afterInteractive"
+        />
         <Script
           id="fb-pixel"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
@@ -54,18 +65,6 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1201401578841926&ev=PageView&noscript=1"
             />
         </noscript>
-        
-        {children}
-        <Toaster />
-
-        <Script 
-          id="utmify-script"
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          async
-          defer
-        />
       </body>
     </html>
   );
