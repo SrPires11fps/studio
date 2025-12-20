94a2efc9c1a1b4069fbc0f4ae17cad39fffff90e
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 13:41:45 +0000

    aconteceu denovo, disparou múltiplos pageview. bom, pelo menos sabemos q

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,9 +57,10 @@ export default function RootLayout({
         <Script 
           id="utmify-script"
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          strategy="lazyOnload"
           data-utmify-prevent-xcod-sck
           data-utmify-prevent-subids
+          async
+          defer
         />
         
         {children}
