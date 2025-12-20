947c077dcb27a18727a0fb5582a3c997d1f912f6
Contas do Vicente <cromo.tutube@gmail.com>
2025-12-20 12:51:03 +0000

    adicione o código base do pixel do facebook:
    <!-- Meta Pixel Code -->

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Script from 'next/script';
 import React from 'react';
 import { Poppins } from 'next/font/google';
+import FacebookPixel from '@/components/FacebookPixel';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -28,22 +29,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${poppins.variable}`}>
       <body>
-        <Script id="fb-pixel-base" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1201401578841926');
-            fbq('track', 'PageView');
-          `}
-        </Script>
         {children}
         <Toaster />
+        <FacebookPixel />
         <Script
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
@@ -51,14 +39,6 @@ export default function RootLayout({
           async
           defer
         ></Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1201401578841926&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </html>
   );
