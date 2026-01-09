658a392a7b4543311a6f0986c50a512f2c74d67d
Contas do Vicente <cromo.tutube@gmail.com>
2026-01-09 14:57:24 +0000

    mude a subheadline no início da seção de preços para: Escolha o melhor p

--- src/components/landing/Pricing.tsx
+++ src/components/landing/Pricing.tsx
@@ -48,7 +48,7 @@ export function Pricing() {
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">Um Investimento Único na Sua Saúde</h2>
             <p className="max-w-[900px] text-foreground/80 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Escolha o melhor plano para você e começe sua nova vida sem dor hoje mesmo.
+              Escolha o melhor plano para você e começe uma nova vida sem dor hoje mesmo.
             </p>
           </div>
         </div>
